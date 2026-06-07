571067bdb38219200ce741bdf1ec02d6e8890e68
iki1uc <entertrainer@web.de>
2026-06-07 23:53:27 +0200

    Add tmAlRun function to validate and run tasks

--- tm/al/run.js
+++ tm/al/run.js
@@ -1,1 +1,9 @@
+import { enRun } from '../../en/core.js';
+import { x4Check } from '../../x4/core.js';
+
+export function tmAlRun(task) {
+  const norm = x4Check(task);
+  if (!norm.valid) return { ok: false, err: 'X4 invalid' };
+  return enRun({ mode: '4ALL', task, norm });
+}
 
